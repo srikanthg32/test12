5fc7e04abdb072da3baa41a1cb8dfb30acd75a61
Raja M - Head Office - Corporate - IS <rajam@wepindia.com>
2019-08-28 10:53:02 +0530

    new changes

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace WebApplication4.Controllers
 {
     public class HomeController : Controller
-    {
+    
         public ActionResult Index()
         {
             return View();
@@ -15,7 +15,7 @@ namespace WebApplication4.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = " Description Your application description page.";
 
             return View();
         }
